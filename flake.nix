11dfc272ee919c3c0cfd491b65c8d578668df61c
YUXIN YE <yye@pay.com.au>
2023-11-25 20:10:27 +1100

    make chipM2 work

--- flake.nix
+++ flake.nix
@@ -9,18 +9,13 @@
     defaultPackage.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.stdenv.mkDerivation {
       name = "test-environment";
       buildInputs = with nixpkgs.legacyPackages.aarch64-darwin; [
-        google-chrome
         (vscode-with-extensions.override {
           vscodeExtensions = with vscode-extensions; [
             graphql.vscode-graphql
-            "4ops".terraform
           ];
         })
         docker
-        cypress
         iterm2
-        oh-my-zsh
-        caffeine-ng
       ];
     };
   };
